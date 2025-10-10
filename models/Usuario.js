677710597cc0eebfa78f597da08dd77e66e3871b
juanes079 <juanes079@gmail.com>
2025-10-10 09:58:36 -0500

    feat: añadir modelo de ususario

---
+++ models/Usuario.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model("Users", userSchema);
