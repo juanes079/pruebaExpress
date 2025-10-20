bd745a5ca386eeb2b6bfd7d2c7210468523d1ad6
juanes079 <juanes079@gmail.com>
2025-10-20 10:51:27 -0500

    feat: add createUser and searchUserById services

--- services/usuarios.services.js
+++ services/usuarios.services.js
@@ -1,11 +1,37 @@
-import Usuario from "../models/Usuario.js";
+import UsuarioModels from "../models/Usuario.js";
 
 /* servicio para obtener todos los usuarios */
 export async function getUsuarios() {
   try {
-    const usuarios = await Usuario.findAll();
+    const usuarios = await UsuarioModels.find();
     return usuarios;
   } catch (error) {
     throw new Error("Error al obtener los usuarios: " + error.message);
+  }
+}
+
+export async function createUsuario(usuarioGuardar) {
+  const { email, password } = usuarioGuardar;
+  try {
+    const usuarioExistente = await UsuarioModels.findOne({ email });
+    if (usuarioExistente) {
+      return `ya existe un usuario con el email: ${email}`;
+    }
+    const newUsuario = UsuarioModels.create({ email, password });
+    return newUsuario;
+  } catch (error) {
+    throw new Error("Error al crear nuevo usuario " + error.message);
+  }
+}
+
+export async function buscarUsuario(idUsuario) {
+  try {
+    const buscarUsuario = await UsuarioModels.findById(idUsuario);
+    if (!buscarUsuario) {
+      return `usuario  con el  id:${idUsuario} no existente`;
+    }
+    return buscarUsuario
+  } catch (error) {
+    throw new Error("Error al buscar por id usuario " + error.message);
   }
 }
