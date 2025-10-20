ca762d44a4df93c3e1df97c11798c24cdaba408d
juanes079 <juanes079@gmail.com>
2025-10-20 11:01:50 -0500

    feat: add specific controllers acording with the endPoints

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import {obtenerUsuarios,createUsuarioControllers,buscarUsuarioIdController } from "../controllers/usuarios.controllers.js";
 const router = express.Router();
-router.get("/get-users", (req, res) => res.send("Usuario route works!/get"));
-router.get("/get-oner/:id", (req, res) => res.send("Usuario route works!/get-one"));
-router.post("/create-users", (req, res) => res.send("Usuario route works!/post"));
+router.get("/get-users", obtenerUsuarios);
+router.get("/get-oner/:id", buscarUsuarioIdController);
+router.post("/create-users", createUsuarioControllers);
 router.delete("/delete-users", (req, res) => res.send("Usuario route works!/delete"));
 router.delete("/delete-oner/:id", (req, res) => res.send("Usuario route works!/delete-one"));
 router.patch("/update-oner/:id", (req, res) => res.send("Usuario route works!/patch"));
