f7f228a46d077de66f1229b92e8670e415e7fbd0
juanes079 <juanes079@gmail.com>
2025-10-27 20:36:34 -0500

    feat: implement controllers that are executed by routes

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import {obtenerUsuarios,createUsuarioControllers,buscarUsuarioIdController } from "../controllers/usuarios.controllers.js";
+import {obtenerUsuarios,createUsuarioControllers,buscarUsuarioIdController, usersDelete1, eliminarUsuario,actualizarUsuario} from "../controllers/usuarios.controllers.js";
 const router = express.Router();
 router.get("/get-users", obtenerUsuarios);
 router.get("/get-oner/:id", buscarUsuarioIdController);
 router.post("/create-users", createUsuarioControllers);
-router.delete("/delete-users", (req, res) => res.send("Usuario route works!/delete"));
-router.delete("/delete-oner/:id", (req, res) => res.send("Usuario route works!/delete-one"));
-router.patch("/update-oner/:id", (req, res) => res.send("Usuario route works!/patch"));
+router.delete("/delete-users", usersDelete1);
+router.delete("/delete-oner/:id",eliminarUsuario);
+router.patch("/update-oner/:id",actualizarUsuario);
 
 export default router;
