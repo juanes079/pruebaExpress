8c25dab86b66a9f2f1e916d069c4c16e5d5efaf5
juanes079 <juanes079@gmail.com>
2025-10-27 20:40:25 -0500

    feat: implent users services

--- services/usuarios.services.js
+++ services/usuarios.services.js
@@ -30,8 +30,54 @@ export async function buscarUsuario(idUsuario) {
     if (!buscarUsuario) {
       return `usuario  con el  id:${idUsuario} no existente`;
     }
-    return buscarUsuario
+    return buscarUsuario;
   } catch (error) {
     throw new Error("Error al buscar por id usuario " + error.message);
+  }
+}
+
+export async function deleteUsers() {
+  try {
+    const usersDelete = await UsuarioModels.deleteMany();
+    return {
+      message: " todos los usuarios eliminados de la base de datos ",
+      usersDelete,
+    };
+  } catch (error) {
+    throw new Error(
+      "Error al eliminar los usuarios de la base de datos " + error.message
+    );
+  }
+}
+
+export async function deleteOner(idUsuario) {
+  try {
+    const onerUser = UsuarioModels.findById(idUsuario);
+    if (!onerUser) {
+      return `usuario  con el  id:${idUsuario} no existente`;
+    }
+    const userDelete = await UsuarioModels.deleteOne(onerUser);
+    return `el usuario con el id : ${idUsuario} fue eliminado ${userDelete}exitosamente`;
+  } catch (error) {
+    throw new Error(
+      "Error al eliminar el usuario de la base de datos " + error.message
+    );
+  }
+}
+
+export async function updateUser(idUsuario, usuarioData) {
+  try {
+    const userUpDate = await UsuarioModels.findById(idUsuario);
+    if (userUpDate) {
+      userUpDate.email = usuarioData.email || userUpDate.email;
+      userUpDate.password = usuarioData.password || userUpDate.password;
+      await userUpDate.save();
+      return `el usuario: ${userUpDate} se actualizo`
+    }
+    return `el usuario con el id:${idUsuario} no existe en la base de datos `
+  } catch (error) {
+    throw new Error(
+      "Error al actualizar el usuario en la base de datos " + error.message
+    );
   }
 }
