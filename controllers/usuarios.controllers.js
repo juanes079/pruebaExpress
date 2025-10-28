4190425c3bde6bf99f1f7ce92ae104c1326e7a1e
juanes079 <juanes079@gmail.com>
2025-10-27 20:31:20 -0500

    feat: implement users controllers

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -2,6 +2,9 @@ import {
   getUsuarios,
   createUsuario,
   buscarUsuario,
+  deleteUsers,
+  deleteOner,
+  updateUser,
 } from "../services/usuarios.services.js";
 
 export async function obtenerUsuarios(req, res) {
@@ -29,7 +32,44 @@ export async function buscarUsuarioIdController(req, res) {
     res.status(200).json(usuarioEncontrado);
   } catch (error) {
     res.status(500).json({
-        message: `error al tratar de encontrar usuario indicado con el id: ${req.params.id}`,
-      });
+      message: `error al tratar de encontrar usuario indicado con el id: ${req.params.id}`,
+    });
   }
+}
+
+export async function usersDelete1(req, res) {
+  try {
+    const usuariosEliminados = await deleteUsers();
+    res.status(200).json(usuariosEliminados);
+  } catch (error) {
+    res.status(500).json({
+      message: `error al tratar eliminar los usuarios de la base de datos ${error.message}`,
+    });
+  }
+}
+
+export async function eliminarUsuario(req, res) {
+  try {
+    const { id } = req.params;
+    const usuarioEliminado = await deleteOner(id);
+    res.status(200).json(usuarioEliminado);
+  } catch (error) {
+    res.status(500).json({
+      message: `error al tratar eliminar el usuario de la base de datos ${error.message}`,
+    });
+  }
+}
+export async function actualizarUsuario(req, res) {
+  try {
+    const { id } = req.params;
+    const usuarioData = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const saveUser = await updateUser(id, usuarioData);
+    res.status(200).json(saveUser);
+    
+  } catch (error) {res.status(500).json({
+      message: `error al tratar de actualizar el usuario ${error.message}`,
+    });}
 }
