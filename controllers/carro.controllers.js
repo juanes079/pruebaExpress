81af9d6b4690734cf6876e714553cad5c5b51fd7
juanes079 <juanes079@gmail.com>
2025-11-05 19:27:32 -0500

    feat: add new functions

--- controllers/carro.controllers.js
+++ controllers/carro.controllers.js
@@ -2,6 +2,9 @@ import {
   getAllCars,
   createCar,
   findCarById,
+  deleteAllCars,
+  deleteCar,
+  updateCars,
 } from "../services/carro.services.js";
 
 export async function obtenerCarros(req, res) {
@@ -33,11 +36,52 @@ export async function buscarCarroId(req, res) {
     const carroEncontrado = await findCarById(req.params.id);
     res.status(200).json({
       message: `la peticion se ejecuto exitosamente`,
-      car: carroEncontrado
+      car: carroEncontrado,
     });
   } catch (error) {
     res.status(500).json({
       message: `error al tratar de encontrar carro indicado con el id: ${req.params.id}`,
+    });
+  }
+}
+
+export async function eliminarCarros(req, res) {
+  try {
+    const carrosEliminados = await deleteAllCars();
+    res.status(200).json({
+      message: `todos los carros fueron eliminados correctamente`,
+      cars: carrosEliminados,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+export async function eliminarCarro(req, res) {
+  try {
+    const { id } = req.params;
+    const carroEliminado = await deleteCar(id);
+    res.status(200).json(carroEliminado);
+  } catch (error) {
+    res.status(500).json({
+      message: `error al tratar eliminar el carro de la base de datos ${error.message}`,
+    });
+  }
+}
+
+export async function actualizarCarro(req, res) {
+  try {
+    const { id } = req.params;
+    const carData = {
+      marca: req.body.marca,
+      placa: req.body.placa,
+      año: req.body.año,
+    };
+    const saveCar = await updateCars(id, carData);
+    res.status(200).json(saveCar);
+  } catch (error) {
+    res.status(500).json({
+      message: `error al tratar de actualizar el carro ${error.message}`,
     });
   }
 }
